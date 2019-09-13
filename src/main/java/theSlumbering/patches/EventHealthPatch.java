293d85b6e0a68d3a839613ee32651077c67af5b4
nferris165 <51347535+nferris165@users.noreply.github.com>
2019-09-13 17:13:23 -0400

    patching events
    patched mushrooms event
    patched scrap ooze event
    patched shining light event
    patched cleric event
    patched world of goop event

--- src/main/java/theSlumbering/patches/EventHealthPatch.java
+++ src/main/java/theSlumbering/patches/EventHealthPatch.java
@@ -7,8 +7,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard.CardRarity;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.events.exordium.BigFish;
-import com.megacrit.cardcrawl.events.exordium.GoldenIdolEvent;
+import com.megacrit.cardcrawl.events.exordium.*;
 import com.megacrit.cardcrawl.events.shrines.*;
 import javassist.CannotCompileException;
 import javassist.CtBehavior;
@@ -291,6 +290,231 @@ public class EventHealthPatch {
                         }
                     }
                     __instance.imageEventText.updateDialogOption(2, OPTIONS[12] + OPTIONS[13]);
+                }
+            }
+        }
+    }
+
+    //Mushrooms
+    @SpirePatch(
+            clz = Mushrooms.class,
+            method = "buttonEffect"
+    )
+
+    public static class MushroomsEventPatch {
+        private static int value = -4;
+        @SpireInsertPatch(
+                localvars = {"healAmt"},
+                locator = HealLocator.class
+        )
+        public static void Insert(Mushrooms __instance, int buttonPressed, @ByRef int[] healAmt) {
+            if (AbstractDungeon.player instanceof TheSlumbering) {
+                healAmt[0] = 0;
+                SlumberingMod.decHeartCollectorRelic(value);
+            }
+        }
+
+        public static void Postfix(Mushrooms __instance, int buttonPressed) {
+            if (AbstractDungeon.player instanceof TheSlumbering) {
+                if(buttonPressed == 1){
+                    __instance.roomEventText.updateBodyText(TEXT[6]);
+                }
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz = Mushrooms.class,
+            method = SpirePatch.CONSTRUCTOR
+    )
+
+    public static class MushroomsDescPatch {
+        public static void Postfix(Mushrooms __instance) {
+            if (AbstractDungeon.player instanceof TheSlumbering) {
+                if(__instance.roomEventText.optionList.size() == 4){
+                    __instance.roomEventText.removeDialogOption(3);
+                    __instance.roomEventText.removeDialogOption(2);
+                }
+                if(__instance.roomEventText.optionList.size() != 1){
+                    __instance.roomEventText.updateDialogOption(1, OPTIONS[14]);
+                }
+            }
+        }
+    }
+
+    //Scrap Ooze
+    @SpirePatch(
+            clz = ScrapOoze.class,
+            method = "buttonEffect"
+    )
+
+    public static class ScrapOozeEventPatch {
+        private static int value = 1;
+        @SpireInsertPatch(
+                localvars = {"dmg"},
+                locator = DamageLocator.class
+        )
+        public static void Insert(ScrapOoze __instance, int buttonPressed, @ByRef int[] dmg) {
+            if (AbstractDungeon.player instanceof TheSlumbering) {
+                dmg[0] = 0;
+                SlumberingMod.decHeartCollectorRelic(value);
+            }
+        }
+
+        public static void Postfix(ScrapOoze __instance, int buttonPressed) {
+            if (AbstractDungeon.player instanceof TheSlumbering) {
+                if(__instance.imageEventText.optionList.size() == 2){
+                    if(AbstractDungeon.player.hasRelic(HeartCollector.ID)){
+                        if(AbstractDungeon.player.getRelic(HeartCollector.ID).counter < value){
+                            __instance.imageEventText.updateDialogOption(0, OPTIONS[5], true);
+                        }
+                        else{
+                            int relicObtainChance = (int) ReflectionHacks.getPrivate(
+                                    __instance, ScrapOoze.class, "relicObtainChance");
+                            __instance.imageEventText.updateDialogOption(0, OPTIONS[15] + relicObtainChance + OPTIONS[16]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz = ScrapOoze.class,
+            method = SpirePatch.CONSTRUCTOR
+    )
+
+    public static class ScrapOozeDescPatch {
+        public static void Postfix(ScrapOoze __instance) {
+            if (AbstractDungeon.player instanceof TheSlumbering) {
+                if(AbstractDungeon.player.getRelic(HeartCollector.ID).counter < 1){
+                    __instance.imageEventText.updateDialogOption(0, OPTIONS[5], true);
+                }
+                else {
+                    int relicObtainChance = (int) ReflectionHacks.getPrivate(
+                            __instance, ScrapOoze.class, "relicObtainChance");
+                    __instance.imageEventText.updateDialogOption(0, OPTIONS[15] + relicObtainChance + OPTIONS[16]);
+                }
+            }
+        }
+    }
+
+    //Shining Light
+    @SpirePatch(
+            clz = ShiningLight.class,
+            method = "buttonEffect"
+    )
+
+    public static class ShiningLightEventPatch {
+        private static int value = 3;
+        @SpireInsertPatch(
+                localvars = {"damage"},
+                locator = DamageLocator.class
+        )
+        public static void Insert(ShiningLight __instance, int buttonPressed, @ByRef int[] damage) {
+            if (AbstractDungeon.player instanceof TheSlumbering) {
+                damage[0] = 0;
+                SlumberingMod.decHeartCollectorRelic(value);
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz = ShiningLight.class,
+            method = SpirePatch.CONSTRUCTOR
+    )
+
+    public static class ShiningLightDescPatch {
+        private static int value = 3;
+        public static void Postfix(ShiningLight __instance) {
+            if (AbstractDungeon.player instanceof TheSlumbering) {
+                if(AbstractDungeon.player.getRelic(HeartCollector.ID).counter < value){
+                    __instance.imageEventText.updateDialogOption(0, OPTIONS[5], true);
+                }
+                else {
+                    __instance.imageEventText.updateDialogOption(0, OPTIONS[17] + value + " #rCollected #rHearts.");
+                }
+            }
+        }
+    }
+
+    //Cleric
+    @SpirePatch(
+            clz = Cleric.class,
+            method = "buttonEffect"
+    )
+
+    public static class ClericEventPatch {
+        private static int value = -2;
+        @SpireInsertPatch(
+                localvars = {"healAmt"},
+                locator = HealLocator.class
+        )
+        public static void Insert(Cleric __instance, int buttonPressed, @ByRef int[] healAmt) {
+            if (AbstractDungeon.player instanceof TheSlumbering) {
+                healAmt[0] = 0;
+                SlumberingMod.decHeartCollectorRelic(value);
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz = Cleric.class,
+            method = SpirePatch.CONSTRUCTOR
+    )
+
+    public static class ClericDescPatch {
+        public static void Postfix(Cleric __instance) {
+            if (AbstractDungeon.player instanceof TheSlumbering) {
+                if(AbstractDungeon.player.gold > 35){
+                    __instance.imageEventText.updateDialogOption(0, OPTIONS[18] + 2 + " #gCollected #gHearts.");
+                }
+            }
+        }
+    }
+
+    //World of Goop
+    @SpirePatch(
+            clz = GoopPuddle.class,
+            method = "buttonEffect"
+    )
+
+    public static class GoopPuddleEventPatch {
+        private static int value = 1;
+        @SpireInsertPatch(
+                localvars = {"damage"},
+                locator = DamageLocator.class
+        )
+        public static void Insert(GoopPuddle __instance, int buttonPressed, @ByRef int[] damage) {
+            if (AbstractDungeon.player instanceof TheSlumbering) {
+                damage[0] = 0;
+                SlumberingMod.decHeartCollectorRelic(value);
+            }
+        }
+
+        public static void Postfix(GoopPuddle __instance, int buttonPressed) {
+            if (AbstractDungeon.player instanceof TheSlumbering) {
+                if(buttonPressed == 0){
+                    __instance.imageEventText.updateBodyText(TEXT[7]);
+                }
+            }
+        }
+    }
+
+    @SpirePatch(
+            clz = GoopPuddle.class,
+            method = SpirePatch.CONSTRUCTOR
+    )
+
+    public static class GoopPuddleDescPatch {
+        private static int value = 1;
+        public static void Postfix(GoopPuddle __instance) {
+            if (AbstractDungeon.player instanceof TheSlumbering) {
+                if(AbstractDungeon.player.getRelic(HeartCollector.ID).counter < value){
+                    __instance.imageEventText.updateDialogOption(0, OPTIONS[5], true);
+                }
+                else {
+                    __instance.imageEventText.updateDialogOption(0, OPTIONS[19] + value + " #rCollected #rHeart.");
                 }
             }
         }
